c7ac9b6fd9c687a54876b27ad6bb9b7a6fc9f5e7
Andrew Pierce <ajpierce1@dmacc.edu>
2023-01-24 21:08:16 -0600

    Final

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+# help(list)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+list_one = ['Jack Burton', 'Ellen Ripley', 'Korben Dallas']
+print('Initial:')
+print(list_one)
 
+list_one.append('Leeloo Dallas')
+print('Append:')
+print(list_one)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+list_two = list(list_one)
+print('Copy:')
+print(list_one)
+print(list_two)
 
+print('Index: "Jack Burton"')
+print(list_one.index('Jack Burton'))
+# Causes an error
+# print(list_one.index('leeloo'))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print('Count: "Leeloo Dallas"')
+list_one.append('Leeloo Dallas')
+print(list_one.count('Leeloo Dallas'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('Insert: "list_one.insert(1, \'John Nada\')"')
+list_one.insert(1, 'John Nada')
+print(list_one)
+
+print('Remove: "list_one.remove(\'John Nada\')"')
+list_one.remove('John Nada')
+print(list_one)
+
+print('Reverse:')
+list_one.reverse()
+print(list_one)
+
+print('Sort:')
+list_one.sort()
+print(list_one)
+
+print('Clear:')
+list_one.clear()
+print(list_one)
